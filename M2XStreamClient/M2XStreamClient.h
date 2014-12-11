5cfe203c425b7b7b48327dbd82bd5a3cb32d4a77
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2014-12-11 10:31:09 +0800

    Rename HEX to TO_HEX to avoid name collision

--- M2XStreamClient/M2XStreamClient.h
+++ M2XStreamClient/M2XStreamClient.h
@@ -38,7 +38,7 @@
 #define DBGLNEND
 #endif  // DEBUG
 
-#define HEX(t_) ((char) (((t_) > 9) ? ((t_) - 10 + 'A') : ((t_) + '0')))
+#define TO_HEX(t_) ((char) (((t_) > 9) ? ((t_) - 10 + 'A') : ((t_) + '0')))
 #define MAX_DOUBLE_DIGITS 7
 
 static const int E_OK = 0;
