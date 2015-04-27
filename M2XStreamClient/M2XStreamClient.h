d11448afa88e358fb9cc6841b12009e0f4eed3da
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2015-04-27 23:53:18 +0800

    Release new version

--- M2XStreamClient/M2XStreamClient.h
+++ M2XStreamClient/M2XStreamClient.h
@@ -8,7 +8,7 @@
 #ifdef ARDUINO_PLATFORM
 #include "Arduino.h"
 
-#define USER_AGENT "User-Agent: M2X Arduino Client/2.0.0"
+#define USER_AGENT "User-Agent: M2X Arduino Client/2.0.2"
 #endif
 
 #ifdef MBED_PLATFORM
