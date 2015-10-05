198646a499d40c8b43415685e6862224841bebe1
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2015-10-05 10:26:30 +0800

    New header only Arduino library organization

---
+++ M2XStreamClient/m2x-arduino.h
@@ -0,0 +1,8 @@
+#include "Arduino.h"
+#define USER_AGENT "User-Agent: M2X Arduino Client/2.2.0"
+
+#ifdef DEBUG
+#define DBG(fmt_, data_) Serial.print(data_)
+#define DBGLN(fmt_, data_) Serial.println(data_)
+#define DBGLNEND Serial.println()
+#endif  /* DEBUG */
